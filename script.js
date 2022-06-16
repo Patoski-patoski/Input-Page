bef515917a6cef8642f2ca1af4c1fcf33a657a83
Okafor Patrick <CodesByPatrick>
2022-06-16 14:23:16 +0100

    final

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+document.querySelector("button").addEventListener("click", () => {
+    const input = document.querySelector(".input");
+    const label = document.querySelector(".dormant");
+    const label2 = document.querySelector(".dormant2");
+    const label3 = document.querySelector(".dormant3");
+    const label4 = document.querySelector(".dormant4");
+  if (input.value === "") {
+    label.classList.add("active");
+  } else if (input.value !== "") {
+    label.classList.remove("active");
+  }
+  if (input.value === "") {
+    label2.classList.add("active");
+  } else if (input.value !== "") {
+    label.classList.remove("active");
+  }
+  if (input.value === "") {
+    label3.classList.add("active");
+  } else if (input.value !== "") {
+    label3.classList.remove("active");
+  }
+  if (input.value === "") {
+    label4.classList.add("active");
+  } else if (input.value !== "") {
+    label4.classList.remove("active");
+  }
+  
+});
+
+// function myButton() {
+//     const button = document.querySelector("button");
+//     button.classList.toggle("active");
+// }
